b7fa9a22337883ad2d6565b800717a043d490084
sina-lib <mszandian@aut.ac.ir>
2021-02-03 23:13:35 +0000

    Load Cifar10 and show 9 pics form it

--- main.py
+++ main.py
@@ -2,32 +2,38 @@ import tensorflow as tf
 from tensorflow.keras import datasets,layers, models
 import matplotlib.pyplot as plt
 import numpy as np
+import pickle
 
 # from sklearn.datasets import fetch_mldata
 
+def Load_Cifar10():
+    with open('DS/cifar-10-batches-py/data_batch_1','rb') as fo:
+        dic = pickle.load(fo, encoding='bytes')
 
-import pickle
-with open('DS/cifar-10-batches-py/data_batch_1','rb') as fo:
-    dic = pickle.load(fo, encoding='bytes')
+    data = dic[b'data'] # is already a numpy array
+    labels = dic[b'labels'] # is a list of numbers
+    filenames = dic[b'filenames']
 
-data = dic[b'data'] # is already a numpy array
-labels = dic[b'labels'] # is a list of numbers
-filenames = dic[b'filenames']
+    # reshape all the data
+    data = data.reshape(10000, 3, 32, 32).transpose(0, 2, 3, 1)
 
-print("data contains: ",len(labels),"labels")
-print("filenames: ",filenames[0:3])
-print("labels: ",labels[0:3])
-print("data dimension: ",data[0].shape)
-
-
-# reshape all the data
-data = data.reshape(10000,3,32,32).transpose(0,2,3,1)
+    return data,labels,filenames
 
 
-plt.figure()
 
-for i in range(3):
-    for j in range(3):
-        plt.subplot(3,3,3*i+j+1)
-        plt.imshow(data[3*i+j])
-plt.show()
+
+if __name__ == '__main__':
+    data, labels, filenames = Load_Cifar10()
+
+    print("data contains: ", len(labels), "labels")
+    print("filenames: ", filenames[0:3])
+    print("labels: ", labels[0:3])
+    print("data dimension: ", data[0].shape)
+
+    plt.figure()
+
+    for i in range(3):
+        for j in range(3):
+            plt.subplot(3, 3, 3 * i + j + 1)
+            plt.imshow(data[3 * i + j])
+    plt.show()
